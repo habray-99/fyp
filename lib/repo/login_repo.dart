c5676037a8b18ca3a267d0c4979514403de23e66
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-02-28 19:57:58 +0545

    Add LoginRepo class for handling user login functionality

---
+++ lib/repo/login_repo.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'dart:developer';
+
+// import 'package:file_structure/models/user_details.dart';
+// import 'package:file_structure/utils/apis.dart';
+import 'package:fyp/model/user_detail.dart';
+import 'package:fyp/utils/apis.dart';
+import 'package:http/http.dart' as http;
+
+class LoginRepo {
+  static Future<void> login({
+    required String email,
+    required String password,
+    required Function(Users user, String token) onSuccess,
+    required Function(String message) onError,
+  }) async {
+    try {
+      var header = {
+        "Accept": "application/json",
+      };
+      var body = {
+        "email": email,
+        "password": password,
+        "type": "customer",
+      };
+      http.Response response = await http.post(
+        Uri.parse(Apis.loginUrl),
+        headers: header,
+        body: body,
+      );
+      dynamic data = jsonDecode(response.body);
+      if (response.statusCode == 200 && response.statusCode < 300) {
+        // String token = data["token"].toString();
+        Users user = Users.fromJson(data["user"]);
+        String token = user.memberToken.toString();
+        onSuccess(user, token);
+      } else {
+        onError(data["message"].toString());
+      }
+    } catch (e, s) {
+      log(e.toString());
+      log(s.toString());
+      onError("Something went wrong");
+    }
+  }
+}
