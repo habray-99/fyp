67e827f30cecce9370790b7789190acf91079825
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-02-28 18:49:02 +0545

    Refactor splash screen controller

--- lib/controller/splash_controller.dart
+++ lib/controller/splash_controller.dart
@@ -1,27 +1,29 @@
 import 'dart:async';
 
+import 'package:fyp/controller/core_controller.dart';
+import 'package:fyp/views/auth/login_screen.dart';
 import 'package:get/get.dart';
 
 import '../views/dashboard/home_page.dart';
 
 class SplashScreenController extends GetxController {
-  @override
-  void onInit() {
-    Timer(const Duration(seconds: 4), () async {
-      Get.offAll(const HomePage());
-    });
-    super.onInit();
-  }
-  // final c = Get.put(CoreController());
   // @override
   // void onInit() {
   //   Timer(const Duration(seconds: 4), () async {
-  //     if (c.isUserLoggedIn()) {
-  //       // Get.offAll();
-  //     } else {
-  //       // Get.offAll(LogInScreen());
-  //     }
+  //     Get.offAll(const HomePage());
   //   });
   //   super.onInit();
   // }
+  final c = Get.put(CoreController());
+  @override
+  void onInit() {
+    Timer(const Duration(seconds: 4), () async {
+      if (c.isUserLoggedIn()) {
+        Get.offAll(const HomePage());
+      } else {
+        Get.offAll(const LogInScreen());
+      }
+    });
+    super.onInit();
+  }
 }
