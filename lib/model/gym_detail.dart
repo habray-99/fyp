0e87d62e12361be59744fc40c781c14d444ab414
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-02 19:43:52 +0545

    Refactor Gym class constructor and methods

--- lib/model/gym_detail.dart
+++ lib/model/gym_detail.dart
@@ -1,21 +1,21 @@
 class Gyms {
- final int gymId;
- final String gymName;
- final String gymAddress;
- final String gymPhone;
- final String gymEmail;
- final String gymPhotos;
+  int? gymId;
+  String? gymName;
+  String? gymAddress;
+  String? gymPhone;
+  String? gymEmail;
+  String? gymPhotos;
 
- Gyms({
-    required this.gymId,
-    required this.gymName,
-    required this.gymAddress,
-    required this.gymPhone,
-    required this.gymEmail,
-    required this.gymPhotos,
- });
+  Gyms({
+    this.gymId,
+    this.gymName,
+    this.gymAddress,
+    this.gymPhone,
+    this.gymEmail,
+    this.gymPhotos,
+  });
 
- factory Gyms.fromJson(Map<String, dynamic> json) {
+  factory Gyms.fromJson(Map<String, dynamic> json) {
     return Gyms(
       gymId: json['gym_id'],
       gymName: json['gym_name'],
@@ -24,9 +24,9 @@ class Gyms {
       gymEmail: json['gym_email'],
       gymPhotos: json['gym_photos'],
     );
- }
+  }
 
- Map<String, dynamic> toJson() {
+  Map<String, dynamic> toJson() {
     return {
       'gym_id': gymId,
       'gym_name': gymName,
@@ -35,11 +35,14 @@ class Gyms {
       'gym_email': gymEmail,
       'gym_photos': gymPhotos,
     };
- }
+  }
 }
 
 List<Gyms> gymDetailListFromJson(List<dynamic> gymDetailListJson) =>
-    gymDetailListJson.map((gymDetailJson) => Gyms.fromJson(gymDetailJson as Map<String, dynamic>)).toList();
+    gymDetailListJson
+        .map((gymDetailJson) =>
+            Gyms.fromJson(gymDetailJson as Map<String, dynamic>))
+        .toList();
 
 
 // class GymDetail {
