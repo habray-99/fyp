1bfa15856fc6adaeaf0d7ac2339e08e072cc0796
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-02 20:16:06 +0545

    Update API endpoints

--- lib/utils/apis.dart
+++ lib/utils/apis.dart
@@ -1,12 +1,8 @@
 class Apis {
-  static const baseUrl = "http://192.168.1.77/kenny/api";
-  // static const baseUrl = "http://192.168.1.73/kenny/api";
-  // static const getHistory = "$baseUrl/get_appointments.php/";
-  static const getHistory =
-      "$baseUrl/get_appointments.php?token=02f1711b311e3810e8c367a3a7f24b93";
-  static const getDoctors = "$baseUrl/doctors.php";
-  static const loginUrl = "$baseUrl/login.php";
-  static const registerUrl = "$baseUrl/register_user.php";
-
-  static const String addBooking = "$baseUrl/store_appointments.php";
+  static const baseUrl = "http://localhost/fyp_backend";
+  static const registerUrl = "$baseUrl/api/sign_up.php";
+  static const loginUrl = "$baseUrl/api/login.php";
+  static const updateMemberDetail = "$baseUrl/member/updateMember.php";
+  static const getMemberDetail = "$baseUrl/member/getMember.php";
+  static const paymentDetail = "$baseUrl/payment/registerPayment.php";
 }
