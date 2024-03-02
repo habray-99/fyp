05a12c5f629d7645280dc448ee8b5f71611f1c3d
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-02 20:07:41 +0545

    Update register method parameters and body

--- lib/repo/register_repo.dart
+++ lib/repo/register_repo.dart
@@ -7,10 +7,18 @@ import 'package:http/http.dart' as http;
 
 class RegisterRepo {
   static Future<void> register({
-    required String email,
-    required String name,
-    required String phoneNumber,
-    required String password,
+    // required String email,
+    // required String name,
+    // required String phoneNumber,
+    // required String password,
+    required String memberName,
+    required String memberEmail,
+    required String memberPhone,
+    required String memberAddress,
+    required String? memberHeight,
+    required String? memberWeight,
+    required String? memberToken,
+    required String? tokenExpiry,
     required Function(String successMessage) onSuccess,
     required Function(String message) onError,
   }) async {
@@ -19,11 +27,13 @@ class RegisterRepo {
         "Accept": "application/json",
       };
       var body = {
-        "name": name,
-        "email": email,
-        "phone": phoneNumber,
-        "password": password,
-        "type": "customer"
+        "memberName" : memberName,
+        "memberEmail" : memberEmail,
+        "memberPhone" : memberPhone,
+        "memberAddress" : memberAddress,
+        "memberHeight" : memberHeight,
+        "memberWeight" : memberWeight,
+        "memberType": "customer"
       };
       http.Response response = await http.post(
         Uri.parse(Apis.registerUrl),
