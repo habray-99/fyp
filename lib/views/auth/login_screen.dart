b83d8ce430248748434cc55e5f99640cc0eaa4de
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-03 11:12:37 +0545

    Add image and update layout in login screen

--- lib/views/auth/login_screen.dart
+++ lib/views/auth/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:fyp/controller/auth/login_controller.dart';
 import 'package:fyp/utils/colors.dart';
@@ -12,9 +13,44 @@ class LogInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SafeArea(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              // const SizedBox(
+              //   height: 300,
+              // child: Image(
+              Image(
+                image: NetworkImage(
+                    "https://images.unsplash.com/photo-1583454110551-21f2fa2afe61?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+              ),
+              // ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 200,
+                child: Column(
+                  children: [
+                    Text(
+                      "Welcome Back",
+                      style: CustomTextStyles.f12W600(color: Colors.black87),
+                    ),
+                    Text(
+                      "Login in to continue",
+                      style: CustomTextStyles.f10W300(color: Colors.black87),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
