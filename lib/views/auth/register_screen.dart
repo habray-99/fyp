5363fc55fdf8e1c3b1677f3fde7c228628f68c5a
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-06 09:59:25 +0545

    Update user authentication logic

--- lib/views/auth/register_screen.dart
+++ lib/views/auth/register_screen.dart
@@ -1,4 +1,6 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:fyp/utils/validators.dart';
 import 'package:fyp/views/auth/login_screen.dart';
 import 'package:fyp/views/dashboard/home_page.dart';
 import 'package:get/get.dart';
@@ -21,25 +23,27 @@ class RegisterScreen extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: SafeArea(
-            child: Column(
-              // mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // const SizedBox(
-                //   height: 20,
-                // ),
-                _gymImgAndGreet(),
-                _nameBuilder(),
-                _emailBuilder(),
-                _phoneBuilder(),
-                _addressBuilder(),
-                _passwordBuilder(),
-                _rePasswordBuilder(),
-                _heightBuilder(),
-                _weightBuilder(),
-                _signUp(),
-                _otherMethods(),
-              ],
+            child: SingleChildScrollView(
+              child: Column(
+                // mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // const SizedBox(
+                  //   height: 20,
+                  // ),
+                  _gymImgAndGreet(),
+                  _nameBuilder(),
+                  _emailBuilder(),
+                  _phoneBuilder(),
+                  _addressBuilder(),
+                  _passwordBuilder(),
+                  _rePasswordBuilder(),
+                  _heightBuilder(),
+                  _weightBuilder(),
+                  _signUp(),
+                  _otherMethods(),
+                ],
+              ),
             ),
           ),
         ),
@@ -50,29 +54,32 @@ class RegisterScreen extends StatelessWidget {
   Widget _gymImgAndGreet() {
     return Column(
       children: [
-        Image.network(
-          "https://images.unsplash.com/photo-1577221084712-45b0445d2b00?q=80&w=1898&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-          // height: 300,
+        ClipRRect(
+          borderRadius: BorderRadius.circular(5), // Adjust the border radius as needed
+          child: CachedNetworkImage(imageUrl:
+            "https://images.unsplash.com/photo-1577221084712-45b0445d2b00?q=80&w=1898&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            // height: 300, // Consider setting a fixed height or using AspectRatio for better control
+            fit: BoxFit.cover, // Use BoxFit.cover to ensure the image covers the entire container
+            placeholder: (context, url) => const CircularProgressIndicator(), // Optional: Show a loading indicator while the image is loading
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+          ),
         ),
-        // const SizedBox(
-        //   height: 20,
-        // ),
         const SizedBox(
-          // height: 200,
-          child: ListTile(
-            title: Text(
-              "Welcome to Gym",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
+          height: 20, // Adjust the height as needed
+        ),
+        const ListTile(
+          title: Text(
+            "Welcome to Gym",
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
             ),
-            subtitle: Text(
-              "Please fill the form to register",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
+          ),
+          subtitle: Text(
+            "Please fill the form to register",
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
           ),
         ),
@@ -80,14 +87,50 @@ class RegisterScreen extends StatelessWidget {
     );
   }
 
+
+  // Widget _gymImgAndGreet() {
+  //   return Column(
+  //     children: [
+  //       Image.network(
+  //         "https://images.unsplash.com/photo-1577221084712-45b0445d2b00?q=80&w=1898&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  //         // height: 300,
+  //       ),
+  //       // const SizedBox(
+  //       //   height: 20,
+  //       // ),
+  //       const SizedBox(
+  //         // height: 200,
+  //         child: ListTile(
+  //           title: Text(
+  //             "Welcome to Gym",
+  //             style: TextStyle(
+  //               fontSize: 30,
+  //               fontWeight: FontWeight.bold,
+  //             ),
+  //           ),
+  //           subtitle: Text(
+  //             "Please fill the form to register",
+  //             style: TextStyle(
+  //               fontSize: 20,
+  //               fontWeight: FontWeight.bold,
+  //             ),
+  //           ),
+  //         ),
+  //       ),
+  //     ],
+  //   );
+  // }
+
   Widget _nameBuilder() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextFormField(
         controller: c.memberNameController,
+        textInputAction: TextInputAction.next,
         decoration: InputDecoration(
           labelText: "Name",
           hintText: "Enter your name",
+          hintStyle: TextStyle(color: Colors.black38),
           // prefixIcon: const Icon(Icons.person),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
@@ -102,9 +145,12 @@ class RegisterScreen extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: TextFormField(
         controller: c.memberEmailController,
+        validator: Validators.checkEmailField,
+        textInputAction: TextInputAction.next,
         decoration: InputDecoration(
           labelText: "Email",
           hintText: "Enter your email",
+          hintStyle: TextStyle(color: Colors.black38),
           // prefixIcon: const Icon(Icons.email),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
@@ -119,9 +165,12 @@ class RegisterScreen extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: TextFormField(
         controller: c.memberPhoneController,
+        validator: Validators.checkPhoneField,
+        textInputAction: TextInputAction.next,
         decoration: InputDecoration(
           labelText: "Phone",
           hintText: "Enter your phone",
+          hintStyle: TextStyle(color: Colors.black38),
           // prefixIcon: const Icon(Icons.phone),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
@@ -136,9 +185,12 @@ class RegisterScreen extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: TextFormField(
         controller: c.memberAddressController,
+        validator: Validators.checkFieldEmpty,
+        textInputAction: TextInputAction.next,
         decoration: InputDecoration(
           labelText: "Address",
           hintText: "Enter your address",
+          hintStyle: TextStyle(color: Colors.black38),
           // prefixIcon: const Icon(Icons.location_on),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
@@ -155,9 +207,12 @@ class RegisterScreen extends StatelessWidget {
         () => TextFormField(
           controller: c.memberPasswordController,
           obscureText: c.passwordObscure.value,
+          textInputAction: TextInputAction.next,
+          validator: Validators.checkPasswordField,
           decoration: InputDecoration(
             labelText: "Password",
             hintText: "Enter your password",
+            hintStyle: TextStyle(color: Colors.black38),
             // prefixIcon: const Icon(Icons.lock),
             suffixIcon: IconButton(
               onPressed: () {
@@ -171,6 +226,7 @@ class RegisterScreen extends StatelessWidget {
               borderRadius: BorderRadius.circular(10),
             ),
           ),
+
         ),
       ),
     );
@@ -183,9 +239,12 @@ class RegisterScreen extends StatelessWidget {
         () => TextFormField(
           controller: c.rePasswordController,
           obscureText: c.passwordObscure2.value,
+          textInputAction: TextInputAction.next,
+          validator: Validators.checkPasswordField,
           decoration: InputDecoration(
-            labelText: "Password",
+            labelText: "Re enter Password",
             hintText: "ReEnter your password",
+            hintStyle: TextStyle(color: Colors.black38),
             // prefixIcon: const Icon(Icons.lock),
             suffixIcon: IconButton(
               onPressed: () {
@@ -226,9 +285,11 @@ class RegisterScreen extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: TextFormField(
         controller: c.memberHeightController,
+        textInputAction: TextInputAction.next,
         decoration: InputDecoration(
           labelText: "Height",
           hintText: "Enter your height",
+          hintStyle: TextStyle(color: Colors.black38),
           // prefixIcon: const Icon(Icons.height),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
@@ -243,9 +304,11 @@ class RegisterScreen extends StatelessWidget {
       padding: const EdgeInsets.all(8.0),
       child: TextFormField(
         controller: c.memberWeightController,
+        textInputAction: TextInputAction.done,
         decoration: InputDecoration(
           labelText: "Weight",
           hintText: "Enter your weight",
+          hintStyle: TextStyle(color: Colors.black38),
           // prefixIcon: const Icon(Icons.line_weight),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(10),
@@ -255,7 +318,7 @@ class RegisterScreen extends StatelessWidget {
     );
   }
 
-  Widget _loginButton() {
+  Widget _signUp() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: CustomElevatedButton(
