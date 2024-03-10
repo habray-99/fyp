3549a1ebe8c953ea1e8098ed83b7c3cc43fc506d
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-10 11:18:55 +0545

    Refactor login function and update API endpoint

--- lib/repo/login_repo.dart
+++ lib/repo/login_repo.dart
@@ -11,28 +11,38 @@ class LoginRepo {
   static Future<void> login({
     required String email,
     required String password,
-    required Function(Users user, String token) onSuccess,
+    required Function(Users user, String? token) onSuccess,
     required Function(String message) onError,
   }) async {
     try {
       var header = {
         "Accept": "application/json",
       };
+
       var body = {
-        "email": email,
-        "password": password,
-        "type": "customer",
+        'email': email,
+        'password': password,
       };
-      http.Response response = await http.post(
-        Uri.parse(Apis.loginUrl),
-        headers: header,
-        body: body,
-      );
+
+      print(body);
+      var url = Uri.http('172.16.18.31', 'fyp_backend/apis/login.php');
+      // "type": "customer",
+      // var response = await http.post(
+      //   // Uri.parse(Apis.loginUrl),
+
+      //   ,
+      //   headers: header,
+      //   body: body,
+      // );
+      print(url);
+      var response = await http.post(url, body: body);
+
       dynamic data = jsonDecode(response.body);
-      if (response.statusCode == 200 && response.statusCode < 300) {
+      print(data);
+      if (response.statusCode >= 200 && response.statusCode < 300) {
         // String token = data["token"].toString();
         Users user = Users.fromJson(data["user"]);
-        String token = user.memberToken.toString();
+        String? token = user.memberToken?.toString();
         onSuccess(user, token);
       } else {
         onError(data["message"].toString());
@@ -40,7 +50,10 @@ class LoginRepo {
     } catch (e, s) {
       log(e.toString());
       log(s.toString());
-      onError("Something went wrong");
+      // String m = $'';
+      String m1 = e.toString();
+      String m2 = s.toString();
+      onError("Something went wrong \n$m1\n $m2");
     }
   }
 }
