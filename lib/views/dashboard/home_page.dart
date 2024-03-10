718ce6e43ea9b2b903581b197e6c1312f314b201
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-10 11:19:26 +0545

    Add log out button to home page

--- lib/views/dashboard/home_page.dart
+++ lib/views/dashboard/home_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:fyp/controller/core_controller.dart';
+import 'package:fyp/widgets/custom/custom_elevated_button.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
+  HomePage({super.key});
+  var c = Get.put(CoreController());
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: Center(child: CustomElevatedButton(title: "Log Out", onTap: () {c.logOut();})),
+    );
   }
 }
