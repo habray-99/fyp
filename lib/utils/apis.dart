fcabce09487faa4e653f749734522905179dfb6f
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-10 11:19:05 +0545

    Update API URLs in Apis class

--- lib/utils/apis.dart
+++ lib/utils/apis.dart
@@ -1,7 +1,8 @@
 class Apis {
-  static const baseUrl = "http://localhost/fyp_backend";
-  static const registerUrl = "$baseUrl/api/sign_up.php";
-  static const loginUrl = "$baseUrl/api/login.php";
+  static const baseUrl = "http://172.16.18.31/fyp_backend";
+  // static const baseUrl = "http://192.168.1.2/fyp_backend";
+  static const registerUrl = "$baseUrl/member/addMember.php";
+  static const loginUrl = "$baseUrl/apis/login.php";
   static const updateMemberDetail = "$baseUrl/member/updateMember.php";
   static const getMemberDetail = "$baseUrl/member/getMember.php";
   static const paymentDetail = "$baseUrl/payment/registerPayment.php";
