9b092d09c809df843a8a9fccafd47c27962fde23
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-13 11:06:58 +0545

    Add drawer to home page

--- lib/views/dashboard/home_page.dart
+++ lib/views/dashboard/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fyp/controller/core_controller.dart';
+import 'package:fyp/views/dashboard/all_gyms_list.dart';
 import 'package:fyp/views/dashboard/quote_list_page.dart';
 import 'package:fyp/widgets/custom/custom_elevated_button.dart';
 import 'package:get/get.dart';
@@ -9,28 +10,78 @@ class HomePage extends StatelessWidget {
   var c = Get.put(CoreController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
-      ),
-      // body: Center(child: CustomElevatedButton(title: "Log Out", onTap: () {c.logOut();})),
-      body: Column(
-      // crossAxisAlignment: 
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
-        children: [
-          CustomElevatedButton(
-            title: "Log Out",
-            onTap: () {
-              c.logOut();
-            },
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home Page"),
+        ),
+        // body: Center(child: CustomElevatedButton(title: "Log Out", onTap: () {c.logOut();})),
+        drawer: Drawer(
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(50),
+              bottomRight: Radius.circular(50),
+            ),
           ),
-          CustomElevatedButton(
-            title: "Quotes",
-            onTap: () {
-              Get.to(() => QuoteListPage());
-            },
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
+          child: ListView(
+            // Important: Remove any padding from the ListView.
+            padding: EdgeInsets.zero,
+            children: [
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Text('Drawer Header'),
+              ),
+              ListTile(
+                title: const Text("Browse Gyms"),
+                onTap: () {
+                  Get.to(() => GymListPage());
+                },
+              ),
+              ListTile(
+                title: const Text('Calories Calculator'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                  Get.to(() => QuoteListPage());
+                },
+              ),
+              ListTile(
+                title: const Text('Generate Exercise Plan'),
+                onTap: () {
+                  // Update the state of the app.
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text("Settings"),
+                onTap: () {},
+              ),
+              ListTile(
+                title: const Text("Logout"),
+                onTap: () {
+                  c.logOut();
+                },
+              )
+            ],
           ),
-        ],
+        ),
+        body: Column(
+          children: [
+            ListTile(
+              title: Text(
+                  "Welcome ${c.currentUser.value?.memberName?.split(' ').first}"),
+              // subtitle: Text(
+              // "You are logged in as ${c.currentUser.value?.memberName}"),
+              // ),
+            ),
+          ],
+        ),
       ),
     );
   }
