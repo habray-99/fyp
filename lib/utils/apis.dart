c64a3cc35446157b6863295fb9ea439980853d44
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-13 11:06:35 +0545

    Update API base URL and add new API endpoint

--- lib/utils/apis.dart
+++ lib/utils/apis.dart
@@ -1,9 +1,12 @@
+// import 'dart:io';
 class Apis {
-  static const baseUrl = "http://172.16.17.225/fyp_backend";
+  static const baseUrl = "http://172.16.18.247/fyp_backend";
   // static const baseUrl = "http://localhost/fyp_backend";
+  // static const baseUrl = "http://172.16.16.1/fyp_backend";
   static const registerUrl = "$baseUrl/member/addMember.php";
   static const loginUrl = "$baseUrl/apis/login.php";
   static const updateMemberDetail = "$baseUrl/member/updateMember.php";
   static const getMemberDetail = "$baseUrl/member/getMember.php";
   static const paymentDetail = "$baseUrl/payment/registerPayment.php";
+  static const getGymsUrl = "$baseUrl/Gym/FetchAllGym.php";
 }
