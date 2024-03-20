6c901805e008a3446fe8898c164a3669071a5e70
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-03-20 16:41:27 +0545

    Update API base URL to use dynamic IP address

--- lib/utils/apis.dart
+++ lib/utils/apis.dart
@@ -1,6 +1,8 @@
 // import 'dart:io';
 class Apis {
-  static const baseUrl = "http://172.16.18.247/fyp_backend";
+  static const ip = "172.16.19.49";
+  // static const ip = "http://localhost";
+  static const baseUrl = "http://$ip/fyp_backend";
   // static const baseUrl = "http://localhost/fyp_backend";
   // static const baseUrl = "http://172.16.16.1/fyp_backend";
   static const registerUrl = "$baseUrl/member/addMember.php";
