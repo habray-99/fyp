4e3022222c776f2923dde569250a9dba84dd0292
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-01 17:13:50 +0545

    Update API endpoints and credentials

--- lib/utils/apis.dart
+++ lib/utils/apis.dart
@@ -1,7 +1,7 @@
 // import 'dart:io';
 class Apis {
-  static const ip = "172.16.18.1";
-  // static const ip = "http://localhost";
+  static const ip = "10.0.2.2";
+  // static const ip = "http://172.16.19.74";
   static const baseUrl = "http://$ip/fyp_backend";
   // static const baseUrl = "http://localhost/fyp_backend";
   // static const baseUrl = "http://172.16.16.1/fyp_backend";
@@ -11,8 +11,13 @@ class Apis {
   static const getMemberDetail = "$baseUrl/member/getMember.php";
   static const paymentDetail = "$baseUrl/payment/registerPayment.php";
   static const getGymsUrl = "$baseUrl/Gym/FetchAllGym.php";
-}
+  static const addBooking = "$baseUrl/payment/registerPayment.php";
 
-const String E_CLIENT_ID =
-    "JB0BBQ4aD0UqIThFJwAKBgAXEUkEGQUBBAwdOgABHD4DChwUAB0R";
-const String E_SECRET_KEY = "BhwIWQQADhIYSxILExMcAgFXFhcOBwAKBgAXEQ==";
+  // static const getRecommendations = "http://10.0.2.2/myapp/random-exercises/";
+  static const getRecommendations =
+      "http://10.0.2.2:8000/myapp/random-exercises/";
+
+  static String get E_CLIENT_ID =>
+      "JB0BBQ4aD0UqIThFJwAKBgAXEUkEGQUBBAwdOgABHD4DChwUAB0R";
+  static String get E_SECRET_KEY => "BhwIWQQADhIYSxILExMcAgFXFhcOBwAKBgAXEQ==";
+}
