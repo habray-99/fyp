9f50d1a5c006e37455c97d1cdf6540c358dba206
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-01 17:15:03 +0545

    Add GetRecommendationsRepo class for fetching exercise recommendations

---
+++ lib/repo/get_recommendation_repo.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:fyp/utils/apis.dart';
+import 'package:http/http.dart' as http;
+
+import '../model/exercises.dart';
+
+class GetRecommendationsRepo {
+  static Future<void> getRecommendations({
+    required String exercisesType,
+    required String exercisesLevel,
+    required int numberOfExercises,
+    required Function(List<ExerciseRecommendations> exerciseRecommendation)
+        onSuccess,
+    required Function(String message) onError,
+  }) async {
+    try {
+      var header = {
+        "Accept": "application/json",
+      };
+
+      // Construct the URL with query parameters
+      var url = Uri.parse(Apis.getRecommendations).replace(queryParameters: {
+        'type': exercisesType,
+        'level': exercisesLevel,
+        'numberOfExercises': numberOfExercises.toString(),
+      });
+
+      log(url.toString());
+
+      var response = await http.get(
+        url,
+        headers: header,
+      );
+
+      dynamic data = jsonDecode(response.body);
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        List<ExerciseRecommendations> recommendations =
+            exerciseRecommendationsListFromJson(data["exercises"]);
+        onSuccess(recommendations);
+      } else {
+        onError(data["message"].toString());
+      }
+    } catch (e, s) {
+      log(e.toString());
+      log(s.toString());
+      onError("Something went wrong.");
+    }
+  }
+}
