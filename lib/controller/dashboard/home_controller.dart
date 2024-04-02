145327772460aa077389ce48525baea441290a94
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-02 11:22:30 +0545

    Commented out registerPayment method in HomeScreenController

--- lib/controller/dashboard/home_controller.dart
+++ lib/controller/dashboard/home_controller.dart
@@ -1,26 +1,25 @@
-import 'package:fyp/repo/buying_membership_repo.dart';
 import 'package:get/get.dart';
 
-import '../../utils/custom_snackbar.dart';
 
 class HomeScreenController extends GetxController {
   RxBool loading = RxBool(false);
-  registerPayment(String gymId, DateTime date, int months, int totalCost) async {
-    loading.value = true;
-    await BookingRepo.addBooking(
-        docId: gymId,
-        date: date.toString(),
-        month: months.toString(),
-        amount: totalCost.toString(),
-        onSuccess: () {
-          loading.value = false;
-          Get.offAllNamed("/home");
-          CustomSnackBar.success(
-              title: "Booking", message: "Booking has been successfully done");
-        },
-        onError: (message) {
-          loading.value = false;
-          CustomSnackBar.error(title: "Booking", message: message);
-        });
-  }
+  // registerPayment(
+  //     String gymId, DateTime date, int months, int totalCost) async {
+  //   loading.value = true;
+  //   await BookingRepo.addBooking(
+  //       gymID: gymId,
+  //       date: date.toString(),
+  //       month: months.toString(),
+  //       amount: totalCost.toString(),
+  //       onSuccess: () {
+  //         loading.value = false;
+  //         Get.offAllNamed("/home");
+  //         CustomSnackBar.success(
+  //             title: "Booking", message: "Booking has been successfully done");
+  //       },
+  //       onError: (message) {
+  //         loading.value = false;
+  //         CustomSnackBar.error(title: "Booking", message: message);
+  //       });
+  // }
 }
