138d97e326116c2af32a30fa0c825839516d921f
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-02 11:23:00 +0545

    Remove unused imports in register_screen.dart

--- lib/views/auth/register_screen.dart
+++ lib/views/auth/register_screen.dart
@@ -1,12 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:fyp/utils/validators.dart';
-import 'package:fyp/views/auth/login_screen.dart';
-import 'package:fyp/views/dashboard/home_page.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 
 import '../../controller/auth/sign_up_controller.dart';
 import '../../widgets/custom/custom_elevated_button.dart';
