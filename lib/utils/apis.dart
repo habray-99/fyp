31527325c7bf0dd3dd4460d77dfb4951a52290f6
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-04 10:48:10 +0545

    Update API URLs and endpoints

--- lib/utils/apis.dart
+++ lib/utils/apis.dart
@@ -1,7 +1,7 @@
 // import 'dart:io';
 class Apis {
   static const ip = "10.0.2.2";
-  // static const ip = "http://172.16.19.74";
+  // static const ip = "192.168.1.4";
   static const baseUrl = "http://$ip/fyp_backend";
   // static const baseUrl = "http://localhost/fyp_backend";
   // static const baseUrl = "http://172.16.16.1/fyp_backend";
@@ -9,13 +9,13 @@ class Apis {
   static const loginUrl = "$baseUrl/apis/login.php";
   static const updateMemberDetail = "$baseUrl/member/updateMember.php";
   static const getMemberDetail = "$baseUrl/member/getMember.php";
-  static const paymentDetail = "$baseUrl/payment/registerPayment.php";
+  static const paymentDetail = "$baseUrl/payment/PaymentDone.php";
   static const getGymsUrl = "$baseUrl/Gym/FetchAllGym.php";
   static const addBooking = "$baseUrl/payment/registerPayment.php";
+  static const GETQOUTES = "$baseUrl/Motivation/MotivationPaginated.php";
 
   // static const getRecommendations = "http://10.0.2.2/myapp/random-exercises/";
-  static const getRecommendations =
-      "http://10.0.2.2:8000/myapp/random-exercises/";
+  static const getRecommendations = "http://$ip:8000/myapp/random-exercises/";
 
   static String get E_CLIENT_ID =>
       "JB0BBQ4aD0UqIThFJwAKBgAXEUkEGQUBBAwdOgABHD4DChwUAB0R";
