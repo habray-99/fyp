d520a9c572f1843e122db8fa2e2a6ecfa303958b
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-04 10:47:54 +0545

    Add QuotesRepo class for fetching motivational quotes

---
+++ lib/repo/qoutes_repo.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:fyp/model/quotes.dart';
+import 'package:fyp/utils/apis.dart';
+import 'package:http/http.dart' as http;
+
+class QuotesRepo {
+  Future<void> getQuotes({
+    required int page,
+    required int limit,
+    required Function(List<Quote> quotes) onSuccess,
+    required Function(String? error) onError,
+  }) async {
+    try {
+      var headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      };
+      // Append pagination parameters to the URL
+      String url = '${Apis.GETQOUTES}?page=$page&limit=$limit';
+      http.Response response = await http.get(
+        Uri.parse(url),
+        headers: headers,
+      );
+
+      dynamic data = json.decode(response.body);
+      log(data.toString());
+      // if (response.statusCode == 200 && response.statusCode < 300) {
+      if (data["message"] == "Motivations fetched successfully") {
+        List<Quote> quotes = (data["Motivations"] as List)
+            .map((item) => Quote.fromJson(item))
+            .toList();
+        onSuccess(quotes);
+      } else {
+        onError(data['message']);
+      }
+    } catch (e) {
+      log(e.toString());
+      onError("Something went wrong");
+    }
+  }
+}
