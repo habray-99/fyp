194c9e3685aa5f4ba7221155fea2ae9131b17e6c
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-04 10:47:39 +0545

    Refactor Quote model and repository

--- lib/model/quotes.dart
+++ lib/model/quotes.dart
@@ -1,27 +1,58 @@
+import 'dart:convert';
+
 class Quote {
-  final String text;
-  final String author;
+    String? quotes;
+    int? quotesId;
+    String? quotesAuthor;
 
-  Quote({required this.text, required this.author});
+    Quote({
+        this.quotes,
+        this.quotesId,
+        this.quotesAuthor,
+    });
+
+    factory Quote.fromRawJson(String str) => Quote.fromJson(json.decode(str));
+
+    String toRawJson() => json.encode(toJson());
+
+    factory Quote.fromJson(Map<String, dynamic> json) => Quote(
+        quotes: json["quotes"],
+        quotesId: json["quotes_id"],
+        quotesAuthor: json["quotes_author"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "quotes": quotes,
+        "quotes_id": quotesId,
+        "quotes_author": quotesAuthor,
+    };
 }
 
-class QuoteRepository {
-  final List<Quote> quotes = [
-    Quote(
-        text: 'The only way to do great work is to love what you do.',
-        author: 'Steve Jobs'),
-    Quote(
-        text:
-            'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.',
-        author: 'Albert Schweitzer'),
-    // Add more quotes here
-  ];
 
-  List<Quote> getQuotes() {
-    return quotes;
-  }
+// class Quote {
+//   final String text;
+//   final String author;
 
-  Quote getQuoteById(int id) {
-    return quotes[id];
-  }
-}
+//   Quote({required this.text, required this.author});
+// }
+
+// class QuoteRepository {
+//   final List<Quote> quotes = [
+//     Quote(
+//         text: 'The only way to do great work is to love what you do.',
+//         author: 'Steve Jobs'),
+//     Quote(
+//         text:
+//             'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.',
+//         author: 'Albert Schweitzer'),
+//     // Add more quotes here
+//   ];
+
+//   List<Quote> getQuotes() {
+//     return quotes;
+//   }
+
+//   Quote getQuoteById(int id) {
+//     return quotes[id];
+//   }
+// }
