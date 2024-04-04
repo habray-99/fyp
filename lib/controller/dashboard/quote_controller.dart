f08e121f5fec8188ad5caf0bfc66f39ef3b5b172
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-04 10:47:33 +0545

    Add QuoteController class with fetchQuotes method

---
+++ lib/controller/dashboard/quote_controller.dart
@@ -0,0 +1,65 @@
+import 'package:fyp/repo/qoutes_repo.dart';
+import 'package:get/get.dart';
+import '../../model/quotes.dart';
+
+class QuoteController extends GetxController {
+  // Define a Rx variable for the current quote
+  Rx<Quote?> currentQuote = Rx<Quote?>(null);
+  int currentPage = 1;
+  int limit = 10; // Number of quotes per page
+
+  // Method to update the current quote
+  void updateQuote(Quote quote) {
+    currentQuote.value = quote;
+    update(); // Notify listeners about the change
+  }
+
+  // Method to clear the current quote
+  void clearQuote() {
+    currentQuote.value = null;
+    update(); // Notify listeners about the change
+  }
+
+  final QuotesRepo getQuotesRepo = QuotesRepo();
+  RxList<Quote> quotes = RxList<Quote>();
+
+  @override
+  void onInit() {
+    super.onInit();
+    fetchQuotes();
+  }
+
+  void fetchQuotes() async {
+    try {
+      await getQuotesRepo.getQuotes(
+        page: 1, // Adjust the page number as needed
+        limit: 10, // Adjust the limit as needed
+        onSuccess: (newQuotes) {
+          quotes.addAll(newQuotes);
+          currentPage++;
+          update();
+        },
+        onError: (error) {
+          // Handle error
+        },
+      );
+    } catch (e) {
+      // Handle error
+    }
+  }
+
+  // Example method to fetch a quote from an API
+//  Future<void> fetchQuote() async {
+//     // Replace this URL with your actual API endpoint
+//     final response = await http.get(Uri.parse('https://api.example.com/quote'));
+
+//     if (response.statusCode == 200) {
+//       // If the server returns a 200 OK response, parse the JSON.
+//       final quote = Quote.fromJson(jsonDecode(response.body));
+//       updateQuote(quote);
+//     } else {
+//       // If the server returns an error response, throw an exception.
+//       throw Exception('Failed to load quote');
+//     }
+//  }
+}
