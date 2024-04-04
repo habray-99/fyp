d8c35fd578829727d41d1f7d309a8853f06ed253
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-04 10:48:36 +0545

    Refactor ExerciseRecommendationPage widget

--- lib/views/dashboard/exercise_recommendation_page.dart
+++ lib/views/dashboard/exercise_recommendation_page.dart
@@ -1,3 +1,37 @@
+// // import 'package:flutter/material.dart';
+// // import '../../model/exercises.dart';
+
+// // class ExerciseRecommendationPage extends StatelessWidget {
+// //   final List<ExerciseRecommendations> recommendations;
+
+// //   const ExerciseRecommendationPage({super.key, required this.recommendations});
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         title: const Text('Exercise Recommendations'),
+// //       ),
+// //       body: ListView.builder(
+// //         itemCount: recommendations.length,
+// //         itemBuilder: (context, index) {
+// //           final recommendation = recommendations[index];
+// //           return ListTile(
+// //             title: Text(recommendation.title ?? ''),
+// //             subtitle: Column(
+// //               crossAxisAlignment: CrossAxisAlignment.start,
+// //               children: [
+// //                 Text("Exercise description: ${recommendation.desc ?? ''}"),
+// //                 Text("Exercise body part: ${recommendation.bodyPart ?? ''}"),
+// //                 Text("Exercise equipment: ${recommendation.equipment ?? ''}"),
+// //               ],
+// //             ),
+// //           );
+// //         },
+// //       ),
+// //     );
+// //   }
+// // }
 // import 'package:flutter/material.dart';
 // import '../../model/exercises.dart';
 
@@ -16,22 +50,46 @@
 //         itemCount: recommendations.length,
 //         itemBuilder: (context, index) {
 //           final recommendation = recommendations[index];
-//           return ListTile(
+//           return ExpansionTile(
+//             expandedCrossAxisAlignment: CrossAxisAlignment.start,
 //             title: Text(recommendation.title ?? ''),
-//             subtitle: Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 Text("Exercise description: ${recommendation.desc ?? ''}"),
-//                 Text("Exercise body part: ${recommendation.bodyPart ?? ''}"),
-//                 Text("Exercise equipment: ${recommendation.equipment ?? ''}"),
-//               ],
-//             ),
+//             children: [
+//               Padding(
+//                 padding:
+//                     const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//                 child: Text(
+//                   "Exercise description: ${recommendation.desc == '' ? 'Currently not Available' : recommendation.desc}",
+//                   // style: const TextStyle()
+//                   textAlign: TextAlign.justify,
+//                   style: const TextStyle(overflow: TextOverflow.ellipsis),
+//                 ),
+//               ),
+//               Padding(
+//                 padding:
+//                     const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//                 child: Text(
+//                   "Exercise body part: ${recommendation.bodyPart == '' ? 'Currently not Available' : recommendation.bodyPart}",
+//                   // style: const TextStyle(fontSize: 16),
+//                   textAlign: TextAlign.justify,
+//                 ),
+//               ),
+//               Padding(
+//                 padding:
+//                     const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+//                 child: Text(
+//                   "Exercise equipment: ${recommendation.equipment == '' ? 'Currently not Available' : recommendation.equipment}",
+//                   // style: const TextStyle(fontSize: 16),
+//                   textAlign: TextAlign.justify,
+//                 ),
+//               ),
+//             ],
 //           );
 //         },
 //       ),
 //     );
 //   }
 // }
+
 import 'package:flutter/material.dart';
 import '../../model/exercises.dart';
 
@@ -50,39 +108,40 @@ class ExerciseRecommendationPage extends StatelessWidget {
         itemCount: recommendations.length,
         itemBuilder: (context, index) {
           final recommendation = recommendations[index];
-          return ExpansionTile(
-            expandedCrossAxisAlignment: CrossAxisAlignment.start,
-            title: Text(recommendation.title ?? ''),
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Text(
-                  "Exercise description: ${recommendation.desc == '' ? 'Currently not Available' : recommendation.desc}",
-                  // style: const TextStyle()
-                  textAlign: TextAlign.justify,
-                  style: const TextStyle(overflow: TextOverflow.ellipsis),
+          return Card(
+            elevation: 4.0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: ExpansionTile(
+              initiallyExpanded: index == 0,
+              title: Text(recommendation.title ?? ''),
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Exercise description: ${recommendation.desc == '' ? 'Currently not Available' : recommendation.desc}",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        "Exercise body part: ${recommendation.bodyPart == '' ? 'Currently not Available' : recommendation.bodyPart}",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      const SizedBox(height: 8.0),
+                      Text(
+                        "Exercise equipment: ${recommendation.equipment == '' ? 'Currently not Available' : recommendation.equipment}",
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Text(
-                  "Exercise body part: ${recommendation.bodyPart == '' ? 'Currently not Available' : recommendation.bodyPart}",
-                  // style: const TextStyle(fontSize: 16),
-                  textAlign: TextAlign.justify,
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-                child: Text(
-                  "Exercise equipment: ${recommendation.equipment == '' ? 'Currently not Available' : recommendation.equipment}",
-                  // style: const TextStyle(fontSize: 16),
-                  textAlign: TextAlign.justify,
-                ),
-              ),
-            ],
+              ],
+            ),
           );
         },
       ),
