d0bdd9ae0606e17d8b9e95d7ce3fb98a47abdf04
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-05 14:00:39 +0545

    Add updateUrl and imageUrl to Apis class

--- lib/utils/apis.dart
+++ lib/utils/apis.dart
@@ -6,7 +6,9 @@ class Apis {
   // static const baseUrl = "http://localhost/fyp_backend";
   // static const baseUrl = "http://172.16.16.1/fyp_backend";
   static const registerUrl = "$baseUrl/member/addMember.php";
+  static const updateUrl = "$baseUrl/member/updateMember.php";
   static const loginUrl = "$baseUrl/apis/login.php";
+  static const imageUrl = "$baseUrl/assets/";
   static const updateMemberDetail = "$baseUrl/member/updateMember.php";
   static const getMemberDetail = "$baseUrl/member/getMember.php";
   static const paymentDetail = "$baseUrl/payment/PaymentDone.php";
