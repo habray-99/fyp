1859d832cc3e5f24039c546f2ad143a2efe32afe
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-05 16:16:15 +0545

    Add MembershipsController to handle fetching and displaying gym memberships

---
+++ lib/controller/dashboard/memberships_controller.dart
@@ -0,0 +1,28 @@
+
+import 'package:fyp/model/memberships.dart';
+import 'package:fyp/repo/get_memberships.dart';
+import 'package:fyp/utils/custom_snackbar.dart';
+import 'package:get/get.dart';
+
+class PaymentsController extends GetxController {
+ // Observable list to hold the gyms data
+ var paymentsList = <Payments>[].obs;
+
+ // Instance of GetGymsRepo
+ final GetMEmbershipsRepo getMembershipssRepo = GetMEmbershipsRepo();
+
+ // Method to fetch gyms data
+ void fetchGyms(int? memberId) async {
+    await getMembershipssRepo.getMemberships(
+      memberId: memberId,
+      onSuccess: (memberships) {
+        paymentsList.value = memberships;
+        CustomSnackBar.success(title: "Memberships", message: "Memberships Fetched Successfully");
+      },
+      onError: (error) {
+        // Handle error
+        CustomSnackBar.error(title: "Memberships", message: "Failed to Fetch Memberships");
+      },
+    );
+ }
+}
