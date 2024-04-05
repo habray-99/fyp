61d719230be4598234387f84a5265e860737e32b
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-05 14:00:32 +0545

    Add image upload functionality to RegisterRepo and updateDetail methods

--- lib/repo/register_repo.dart
+++ lib/repo/register_repo.dart
@@ -1,8 +1,11 @@
 import 'dart:convert';
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:fyp/utils/apis.dart';
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
+import 'package:http_parser/http_parser.dart';
 
 import '../model/user_detail.dart';
 
@@ -18,40 +21,102 @@ class RegisterRepo {
     String? memberType,
     required Function(Users users, String token) onSuccess,
     required Function(String message) onError,
-    String? memberImageUrl,
+    File? memberImageUrl,
   }) async {
     try {
-      var header = {
-        "Accept": "application/json",
-      };
-      var body = {
-        "member_name": memberName,
-        "member_email": memberEmail,
-        "member_phone": memberPhone,
-        "member_password": memberPassword,
-        "member_address": memberAddress,
-        "member_height": memberHeight,
-        "member_weight": memberWeight,
-        "member_type": memberType,
-      };
-      http.Response response = await http.post(
-        Uri.parse(Apis.registerUrl),
-        headers: header,
-        body: body,
-      );
-      log(body.toString());
-      log(Apis.registerUrl);
-      dynamic data = jsonDecode(response.body.toString());
-      // log(data.toString());
-      // if (response.statusCode >= 200 && response.statusCode <= 300) {
+      var uri = Uri.parse(Apis.registerUrl);
+      var request = http.MultipartRequest('POST', uri);
+      request.fields['member_name'] = memberName;
+      request.fields['member_email'] = memberEmail;
+      request.fields['member_phone'] = memberPhone;
+      request.fields['member_password'] = memberPassword;
+      request.fields['member_address'] = memberAddress;
+      request.fields['member_height'] = memberHeight ?? '';
+      request.fields['member_weight'] = memberWeight ?? '';
+      request.fields['member_type'] = memberType ?? "1";
+      if (memberImageUrl != null) {
+        var imageFile = await http.MultipartFile.fromPath(
+          'member_image',
+          memberImageUrl as String,
+          contentType:
+              MediaType('image', path.extension(memberImageUrl as String).substring(1)),
+        );
+        request.files.add(imageFile);
+      }
+
+      var response = await request.send();
       if (response.statusCode == 200) {
+        var responseData = await http.Response.fromStream(response);
+        dynamic data = jsonDecode(responseData.body.toString());
+
         log(data.toString());
         Users user = Users.fromJson(data["member"]);
         String token = user.memberToken.toString();
-        // onSuccess(data["message"]);
         onSuccess(user, token);
       } else {
-        onError(data["message"]);
+        onError('Registration failed');
+      }
+    } catch (e, s) {
+      log(e.toString());
+      log(s.toString());
+      onError("Something went wrong");
+    }
+  }
+
+  static Future<void> updateDetail({
+    required String memberName,
+    required String memberEmail,
+    required String memberPhone,
+    required String memberAddress,
+    String? memberPassword,
+    String? memberHeight,
+    String? memberWeight,
+    String? memberType,
+    required Function(Users users, String token) onSuccess,
+    required Function(String message) onError,
+    String? memberImageUrl,
+    required String memberId,
+  }) async {
+    try {
+      var uri = Uri.parse(Apis.updateMemberDetail);
+      var request = http.MultipartRequest('POST', uri);
+
+      // Add user detail fields
+      request.fields['member_name'] = memberName;
+      request.fields['member_email'] = memberEmail;
+      request.fields['member_phone'] = memberPhone;
+      request.fields['member_id'] = memberId;
+      // request.fields['member_password'] = memberPassword;
+      request.fields['member_address'] = memberAddress;
+      request.fields['member_height'] = memberHeight ?? '';
+      request.fields['member_weight'] = memberWeight ?? '';
+      request.fields['member_type'] = memberType ?? '';
+
+      log("image is : ${memberImageUrl.toString()}");
+
+      // Add image file if available
+      if (memberImageUrl != null) {
+        var imageFile = await http.MultipartFile.fromPath(
+          'member_image',
+          memberImageUrl,
+          contentType:
+              MediaType('image', path.extension(memberImageUrl).substring(1)),
+        );
+        request.files.add(imageFile);
+      }
+
+      var response = await request.send();
+
+      if (response.statusCode == 200) {
+        var responseData = await http.Response.fromStream(response);
+        dynamic data = jsonDecode(responseData.body.toString());
+
+        log(data.toString());
+        Users user = Users.fromJson(data["member"]);
+        String token = user.memberToken.toString();
+        onSuccess(user, token);
+      } else {
+        onError('Update detail failed');
       }
     } catch (e, s) {
       log(e.toString());
