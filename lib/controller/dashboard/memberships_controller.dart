742038a6b72908aeba4aaba1231e9cc44282dfb4
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-09 16:56:19 +0545

    Refactor memberships controller and sort memberships by validity

--- lib/controller/dashboard/memberships_controller.dart
+++ lib/controller/dashboard/memberships_controller.dart
@@ -1,28 +1,30 @@
-
 import 'package:fyp/model/memberships.dart';
 import 'package:fyp/repo/get_memberships.dart';
 import 'package:fyp/utils/custom_snackbar.dart';
 import 'package:get/get.dart';
 
 class PaymentsController extends GetxController {
- // Observable list to hold the gyms data
- var paymentsList = <Payments>[].obs;
+  // Observable list to hold the gyms data
+  var paymentsList = <Payments>[].obs;
 
- // Instance of GetGymsRepo
- final GetMEmbershipsRepo getMembershipssRepo = GetMEmbershipsRepo();
+  // Instance of GetGymsRepo
+  final GetMEmbershipsRepo getMembershipssRepo = GetMEmbershipsRepo();
 
- // Method to fetch gyms data
- void fetchGyms(int? memberId) async {
+  // Method to fetch gyms data
+  void fetchGyms(int? memberId) async {
     await getMembershipssRepo.getMemberships(
       memberId: memberId,
       onSuccess: (memberships) {
         paymentsList.value = memberships;
-        CustomSnackBar.success(title: "Memberships", message: "Memberships Fetched Successfully");
+        memberships.sort((a, b) => b.isValid!.compareTo(a.isValid!));
+        CustomSnackBar.success(
+            title: "Memberships", message: "Memberships Fetched Successfully");
       },
       onError: (error) {
         // Handle error
-        CustomSnackBar.error(title: "Memberships", message: "Failed to Fetch Memberships");
+        CustomSnackBar.error(
+            title: "Memberships", message: "Failed to Fetch Memberships");
       },
     );
- }
+  }
 }
