1fe474951182765b464fb052057eff0051d55abb
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-09 17:52:26 +0545

    Update CustomElevatedButton style and text color

--- lib/widgets/custom/custom_elevated_button.dart
+++ lib/widgets/custom/custom_elevated_button.dart
@@ -25,14 +25,16 @@ class CustomElevatedButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       style: ElevatedButton.styleFrom(
-          backgroundColor: AppColors.borderColor,
+          backgroundColor: const Color.fromRGBO(113, 201, 206, 1),
           minimumSize: Size.fromHeight(height),
           shape:
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(10))),
       onPressed: isDisabled ? null : onTap,
       child: Text(
         title,
-        style: CustomTextStyles.f16W600(color: textColor ?? Colors.white),
+        // style: CustomTextStyles.f16W600(color: textColor ?? Color.fromRGBO(249, 246, 238, 1)),
+        style: CustomTextStyles.f16W600(
+            color: textColor ?? const Color(0xFF343434)),
       ),
     );
   }
