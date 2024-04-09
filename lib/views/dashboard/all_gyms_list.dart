8a0e5db100d73c4124f9373945857f4a9045c691
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-09 17:52:10 +0545

    added search and sort

--- lib/views/dashboard/all_gyms_list.dart
+++ lib/views/dashboard/all_gyms_list.dart
@@ -189,6 +189,9 @@ class GymListPage extends StatefulWidget {
 class _GymListPageState extends State<GymListPage> {
   final GetGymsRepo getGymsRepo = GetGymsRepo();
   List<Gyms> gyms = [];
+  List<Gyms> filteredGyms = [];
+  TextEditingController searchController = TextEditingController();
+  bool isSortedAscending = true;
 
   @override
   void initState() {
@@ -202,6 +205,7 @@ class _GymListPageState extends State<GymListPage> {
         onSuccess: (gymsList) {
           setState(() {
             gyms = gymsList;
+            filteredGyms = gymsList;
           });
         },
         onError: (error) {
@@ -213,75 +217,249 @@ class _GymListPageState extends State<GymListPage> {
     }
   }
 
+  void filterGyms(String query) {
+    setState(() {
+      filteredGyms = gyms
+          .where((gym) =>
+              (gym.gymName?.toLowerCase().contains(query.toLowerCase()) ??
+                  false) ||
+              (gym.gymAddress?.toLowerCase().contains(query.toLowerCase()) ??
+                  false))
+          .toList();
+    });
+  }
+
+  void sortGyms() {
+    setState(() {
+      filteredGyms.sort((a, b) {
+        if (isSortedAscending) {
+          return (a.gymPrice ?? 0).compareTo(b.gymPrice ?? 0);
+        } else {
+          return (b.gymPrice ?? 0).compareTo(a.gymPrice ?? 0);
+        }
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Gyms'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 0.8,
-            crossAxisSpacing: 8.0,
-            mainAxisSpacing: 8.0,
-          ),
-          itemCount: gyms.length,
-          itemBuilder: (context, index) {
-            final gym = gyms[index];
-            return InkWell(
-              onTap: () => Get.to(() => GymDetailPage(gym: gym)),
-              child: Card(
-                elevation: 4.0,
-                shape: RoundedRectangleBorder(
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: 'Search gyms',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(8.0),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Expanded(
-                      child: Hero(
-                        tag: 'gym-${gym.gymId}-image',
-                        child: CachedNetworkImage(
-                          imageUrl:
-                              "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                          placeholder: (context, url) =>
-                              const CircularProgressIndicator(),
-                          errorWidget: (context, url, error) =>
-                              const Icon(Icons.error),
-                          fit: BoxFit.scaleDown,
-                        ),
+              ),
+              onChanged: (value) {
+                filterGyms(value);
+              },
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(
+                icon: const Icon(Icons.sort),
+                onPressed: () {
+                  sortGyms();
+                  setState(() {
+                    isSortedAscending = !isSortedAscending;
+                  });
+                },
+              ),
+              Text(isSortedAscending
+                  ? 'Sort by Price (Ascending)'
+                  : 'Sort by Price (Descending)'),
+            ],
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.8,
+                  crossAxisSpacing: 8.0,
+                  mainAxisSpacing: 8.0,
+                ),
+                itemCount: filteredGyms.length,
+                itemBuilder: (context, index) {
+                  final gym = filteredGyms[index];
+                  return InkWell(
+                    onTap: () => Get.to(() => GymDetailPage(gym: gym)),
+                    child: Card(
+                      elevation: 4.0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Hero(
-                            tag: 'gym-${gym.gymId}-name',
-                            child: Text(
-                              gym.gymName ?? 'Unknown Gym',
-                              style: Theme.of(context).textTheme.headlineMedium,
-                              overflow: TextOverflow.ellipsis,
+                          Expanded(
+                            child: Hero(
+                              tag: 'gym-${gym.gymId}-image',
+                              child: CachedNetworkImage(
+                                imageUrl:
+                                    "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                                placeholder: (context, url) =>
+                                    const CircularProgressIndicator(),
+                                errorWidget: (context, url, error) =>
+                                    const Icon(Icons.error),
+                                fit: BoxFit.scaleDown,
+                              ),
                             ),
                           ),
-                          Text(
-                            gym.gymAddress ?? 'Address not available',
-                            overflow: TextOverflow.ellipsis,
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Hero(
+                                  tag: 'gym-${gym.gymId}-name',
+                                  child: Text(
+                                    gym.gymName ?? 'Unknown Gym',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headlineMedium,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                                Text(
+                                  gym.gymAddress ?? 'Address not available',
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                                Text(
+                                  'Price: Rs.${gym.gymPrice ?? 0}',
+                                  style: Theme.of(context).textTheme.bodySmall,
+                                ),
+                              ],
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ),
+                  );
+                },
               ),
-            );
-          },
-        ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+// class GymListPage extends StatefulWidget {
+//   const GymListPage({super.key});
+
+//   @override
+//   _GymListPageState createState() => _GymListPageState();
+// }
+
+// class _GymListPageState extends State<GymListPage> {
+//   final GetGymsRepo getGymsRepo = GetGymsRepo();
+//   List<Gyms> gyms = [];
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     fetchGyms();
+//   }
+
+//   void fetchGyms() async {
+//     try {
+//       await getGymsRepo.getGyms(
+//         onSuccess: (gymsList) {
+//           setState(() {
+//             gyms = gymsList;
+//           });
+//         },
+//         onError: (error) {
+//           print('Error fetching gyms: $error');
+//         },
+//       );
+//     } catch (e) {
+//       print('Exception: $e');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Gyms'),
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.all(8.0),
+//         child: GridView.builder(
+//           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+//             crossAxisCount: 2,
+//             childAspectRatio: 0.8,
+//             crossAxisSpacing: 8.0,
+//             mainAxisSpacing: 8.0,
+//           ),
+//           itemCount: gyms.length,
+//           itemBuilder: (context, index) {
+//             final gym = gyms[index];
+//             return InkWell(
+//               onTap: () => Get.to(() => GymDetailPage(gym: gym)),
+//               child: Card(
+//                 elevation: 4.0,
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.circular(8.0),
+//                 ),
+//                 child: Column(
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Expanded(
+//                       child: Hero(
+//                         tag: 'gym-${gym.gymId}-image',
+//                         child: CachedNetworkImage(
+//                           imageUrl:
+//                               "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+//                           placeholder: (context, url) =>
+//                               const CircularProgressIndicator(),
+//                           errorWidget: (context, url, error) =>
+//                               const Icon(Icons.error),
+//                           fit: BoxFit.scaleDown,
+//                         ),
+//                       ),
+//                     ),
+//                     Padding(
+//                       padding: const EdgeInsets.all(8.0),
+//                       child: Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Hero(
+//                             tag: 'gym-${gym.gymId}-name',
+//                             child: Text(
+//                               gym.gymName ?? 'Unknown Gym',
+//                               style: Theme.of(context).textTheme.headlineMedium,
+//                               overflow: TextOverflow.ellipsis,
+//                             ),
+//                           ),
+//                           Text(
+//                             gym.gymAddress ?? 'Address not available',
+//                             overflow: TextOverflow.ellipsis,
+//                           ),
+//                         ],
+//                       ),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
