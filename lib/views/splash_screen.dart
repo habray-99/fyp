2b21eea9985e146d439d51c0aba5d9b692595117
habray-99 <vamsha.tamu.a21.2@icp.edu.np>
2024-04-23 08:18:25 +0545

    Update splash screen image size

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -8,27 +8,29 @@ class SplashScreen extends StatelessWidget {
   final c = Get.put(SplashScreenController());
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       // backgroundColor: Colors.grey.shade700,
       body: Column(
         children: [
           Center(
             child: SizedBox(
               child: Image(
-                // image: AssetImage(
+                image: const AssetImage('assets/images/output-onlinepngtools.png'),
                 //   // ImagePath.loginBackGround,
                 //   // CachedNetworkImage(imageUrl: "https://images.unsplash.com/photo-1709403338549-10e6b0e24c3f?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",)
 
                 //   // ImagePath.splashLogo,
                 // ),
-                image: NetworkImage(
-                    "https://images.unsplash.com/photo-1709403338549-10e6b0e24c3f?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
-                height: 200,
-                width: 200,
+                // image: NetworkImage(
+                //     "https://images.unsplash.com/photo-1709403338549-10e6b0e24c3f?q=80&w=1972&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"),
+                // height: 200,
+                // width: 200,
+                height: Get.height * .5,
+                width: Get.width * .5,
               ),
             ),
           ),
-          CircularProgressIndicator()
+          const CircularProgressIndicator()
         ],
       ),
     );
